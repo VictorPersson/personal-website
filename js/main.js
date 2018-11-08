b25df5ca5fa0140eaa97f6c7766564cced68373e
VictorPersson <vp-96@hotmail.com>
2018-11-08 18:20:16 +0100

    Implemented jQuery DOM liner gradient change on event

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
-$(document).ready(function() {
-    
-    $("#jsRecolor").click(function() {
-        $(".row").hide();
+$(document).ready(function () {
+
+    $(".section-1").click(function () {
+        $('.section-1').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+        });
     });
 });
