5e87268c7f8dca064387dc90c491c17442a60e90
VictorPersson <vp-96@hotmail.com>
2018-11-08 23:29:38 +0100

    Added more jQuerry color changers, started work on background-blend-mode change fix

--- js/main.js
+++ js/main.js
@@ -4,5 +4,18 @@ $(document).ready(function () {
         $('.section-1').css({
             background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
         });
+
+        $('.footer').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+        });
+        $('.card__side--back').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+        });
+        $('.card__image').css({
+            background-blend - mode: 'screen;'
+        });
+        $('.card__image--1').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+        });
     });
 });
