016ffe6dd4c3ee0104ee3629e940877df6423184
VictorPersson <vp-96@hotmail.com>
2018-11-09 09:33:49 +0100

    Added alt color of logo, implemented jQuery JS script to change site colors

--- js/main.js
+++ js/main.js
@@ -1,21 +1,46 @@
 $(document).ready(function () {
 
-    $(".section-1").click(function () {
-        $('.section-1').css({
-            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
-        });
+    const color1 = "#ff4455"
+    const color2 = "#ff8899"
 
+    $(".section-1").click(function () {
+        $('.heading__front--first').css({
+            color: color1
+        });
+        $('.heading__front--second').css({
+            color: color1
+        });
+        $('.heading-secondary').css({
+            color: color1
+        });
+        $('.button-white').css({
+            color: color1
+        });
+        $(".header__logo").attr('src', "img/v_logo_outside_peach_glow.jpg");
+
+        $('.section-1').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
+        });
         $('.footer').css({
-            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
         $('.card__side--back').css({
-            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
-        $('.card__image').css({
-            background-blend - mode: 'screen;'
-        });
+        $('.card__image').css('background-blend-mode: screen;'
+        );
         $('.card__image--1').css({
-            background: "-webkit-gradient(linear, right top, left bottom, from(#ff4455), to(#ff8899))"
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
+        });
+        $('.card__image--2').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
+        });
+        $('.card__image--3').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
+        });
+        $('.card__image--4').css({
+            background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
     });
 });
+
