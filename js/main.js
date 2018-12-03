c63d8d754b4e0b4a826e2359642dfd823844666e
VictorPersson <vp-96@hotmail.com>
2018-12-03 16:35:28 +0100

    Implemented option to pick JS christmas themes | christmas-2018 update :christmas_tree:

--- js/main.js
+++ js/main.js
@@ -6,34 +6,49 @@ $(document).ready(function () {
     $("#js-button").click(function () {
 
         if (buttonPressed === false) {
-            var color1 = "#c51162"
-            var color2 = "#6d0959"
-            var vLogo = "img/v_logo_outside_peach_glow.jpg"
+            var color1 = "#6DD5FA" // "#c51162"
+            var color2 = "#2980B9" // "#6d0959"
+            var icon_1 = "fas fa-gift"
+            //var vLogo = "img/v_logo_outside_peach_glow.jpg"
             console.log("Button was pressed - is now TRUE")
             buttonPressed = true;
         }
 
         else if (buttonPressed === true) {
-            var color1 = "#ff4455"
-            var color2 = "#ff8899"
+            var color1 = "#972239" // "#ff4455"
+            var color2 = "#e74c3c" // "#ff8899"
+            var icon_2 = "far fa-snowflake"
             var vLogo = "img/v_logo_outside_pink_glow.png"
             console.log("Button was pressed - is now FALSE")
             buttonPressed = false;
         }
 
         $('.heading__front--first').css({
-            color: color1
+            color: color2
         });
         $('.heading__front--second').css({
-            color: color1
+            color: color2
         });
         $('.heading-secondary').css({
-            color: color1
+            color: color2
         });
         $('.button-white').css({
-            color: color1
+            color: color2
         });
-        $(".header__logo").attr('src', "img/v_logo_outside_peach_glow.jpg");
+        $('.card__li-text').css({
+            color: color2
+        });
+        $('.card__header').css({
+            color: color2
+        });
+        $('.card__li-status').css({
+            color: color2
+        });
+        $('.card__link--logo').css({
+            color: color2
+        });
+
+        // $(".header__logo").attr('src', "img/v_logo_outside_peach_glow.jpg");
 
         $('.section-1').css({
             background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
@@ -44,8 +59,6 @@ $(document).ready(function () {
         $('.card__side--back').css({
             background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
-        $('.card__image').css('background-blend-mode: screen;'
-        );
         $('.card__image--1').css({
             background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
@@ -58,6 +71,9 @@ $(document).ready(function () {
         $('.card__image--4').css({
             background: "-webkit-gradient(linear, right top, left bottom, from(" + color1 + "), to(" + color2 + "))"
         });
+        $('.card__image').css('background-blend-mode: screen;'
+        );
+        $("#christmas_icon").removeClass(icon_2).addClass(icon_1);
     });
 });
 
